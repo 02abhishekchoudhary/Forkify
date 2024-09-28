7a7d2b6fa90c249644970df0168e4f911aebd8b4
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2024-09-28 22:52:43 +0530

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c4248d51-c075-4b5b-b7d3-04b3bda90a1e';
+export const MODAL_CLOSE_SEC = 2.5;
